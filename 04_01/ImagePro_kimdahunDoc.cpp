355355ff539e89896d1145f9a3d590c5ff919b51
kdahun <101082485+kdahun@users.noreply.github.com>
2023-04-01 19:21:17 +0900

    Add files via upload

---
+++ 04_01/ImagePro_kimdahunDoc.cpp
@@ -0,0 +1,288 @@
+﻿
+// ImagePro_kimdahunDoc.cpp: CImageProkimdahunDoc 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "ImagePro_kimdahun.h"
+#endif
+#include "ImagePro_kimdahunDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+#pragma warning(suppress : 4996)
+// CImageProkimdahunDoc
+
+IMPLEMENT_DYNCREATE(CImageProkimdahunDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CImageProkimdahunDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CImageProkimdahunDoc 생성/소멸
+
+CImageProkimdahunDoc::CImageProkimdahunDoc() noexcept
+{
+	// TODO: 여기에 일회성 생성 코드를 추가합니다.
+	inputImg = NULL;
+	inputimg2 = NULL;
+	resultImg = NULL;
+	resultImg1 = NULL;
+	resultImg2 = NULL;
+}
+
+CImageProkimdahunDoc::~CImageProkimdahunDoc()
+{
+}
+
+BOOL CImageProkimdahunDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 여기에 재초기화 코드를 추가합니다.
+	// SDI 문서는 이 문서를 다시 사용합니다.
+
+	return TRUE;
+}
+
+
+
+
+// CImageProkimdahunDoc serialization
+
+void CImageProkimdahunDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())//carchive 클래스 안에 isstoring이 있다
+	{
+	}
+	else
+	{
+		// TODO: 여기에 로딩 코드를 추가합니다.
+		LoadImageFile(ar);
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 축소판 그림을 지원합니다.
+void CImageProkimdahunDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 문서의 데이터를 그리려면 이 코드를 수정하십시오.
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 검색 처리기를 지원합니다.
+void CImageProkimdahunDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 문서의 데이터에서 검색 콘텐츠를 설정합니다.
+	// 콘텐츠 부분은 ";"로 구분되어야 합니다.
+
+	// 예: strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void CImageProkimdahunDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CImageProkimdahunDoc 진단
+
+#ifdef _DEBUG
+void CImageProkimdahunDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CImageProkimdahunDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CImageProkimdahunDoc 명령
+
+
+void CImageProkimdahunDoc::PixelAdd()
+{
+	// TODO: 여기에 구현 코드 추가.
+	int value;
+	for (int y = 0; y < 256 ; y++) {
+		for (int x = 0; x < 256; x++) {
+			value = inputImg[y][x] + 100;
+			if (value > 255)resultImg[y][x] = 255;
+			else resultImg[y][x] = value;
+		}
+	}
+}
+
+
+void CImageProkimdahunDoc::Pixeltimes()
+{
+	// TODO: 여기에 구현 코드 추가.
+	int value;
+	for (int y = 0; y < 256; y++) {
+		for (int x = 0; x < 256; x++) {
+			value = inputImg[y][x] * 1.2;
+			if (value > 255)resultImg1[y][x] = 255;
+			else resultImg1[y][x] = value;
+		}
+	}
+}
+
+
+void CImageProkimdahunDoc::PixelHistoEq()
+{
+	// TODO: 여기에 구현 코드 추가.
+	int x, y, i, k;
+	int acc_hist = 0;
+	float N = 256 * 256;
+	int hist[256], sum[256];
+
+	for (k = 0; k < 256; k++)hist[k] = 0;
+	for (y = 0; y < 256; y++) {
+		for (x = 0; x < 256; x++) {
+			k = inputImg[y][x];
+			hist[k] = hist[k] + 1;
+		}
+	}
+	for (i = 0; i < 256; i++) {
+		acc_hist = acc_hist + hist[i];
+		sum[i] = acc_hist;
+	}
+	for (y = 0; y < 256; y++) {
+		for (x = 0; x < 256; x++) {
+			k = inputImg[y][x];
+			resultImg2[y][x] = (unsigned char)(sum[k] / N * 255);
+		}
+	}
+}
+
+
+void CImageProkimdahunDoc::PixelTwoImageAdd()
+{
+
+	// TODO: 여기에 구현 코드 추가.
+	int value;
+	LoadTwoImages();
+
+	for (int y = 0; y < 256; y++) {
+		for (int x = 0; x < 256; x++) {
+			value = inputImg[y][x] + inputimg2[y][x];
+			if (value > 255) resultImg[y][x] = 255;
+			else resultImg[y][x] = value;
+		}
+	}
+}
+
+
+void CImageProkimdahunDoc::LoadTwoImages()
+{
+	// TODO: 여기에 구현 코드 추가.
+	CFile file;
+	CFileDialog dlg(TRUE);
+
+	AfxMessageBox(CString("Select the First Image"));
+
+	if (dlg.DoModal() == IDOK) {
+		file.Open(dlg.GetPathName(), CFile::modeRead);
+		file.Read(inputImg, 256 * 256);
+		file.Close();
+	}
+	AfxMessageBox(CString("Select the Second Image"));
+	if (dlg.DoModal() == IDOK) {
+		file.Open(dlg.GetPathName(), CFile::modeRead);
+		file.Read(inputimg2, 256 * 256);
+		file.Close();
+	}
+}
+
+
+void CImageProkimdahunDoc::LoadImageFile(CArchive& ar)
+{
+	// TODO: 여기에 구현 코드 추가.
+	int i, maxValue;
+	char type[16], buf[256];
+	CFile* fp = ar.GetFile();
+	CString fname = fp->GetFilePath();
+
+	if (strcmp(strrchr(fname, '.'), ".ppm") == 0 ||
+		strcmp(strrchr(fname, '.'), ".PPM") == 0 ||
+		strcmp(strrchr(fname, '.'), ".PGM") == 0 ||
+		strcmp(strrchr(fname, '.'), ".pgm") == 0) {
+		ar.ReadString(type, 15);
+		
+		do {
+			ar.ReadString(buf, 255);
+		} while (buf[0] == '#');
+		sscanf_s(buf, "%d %d", &imageWidth, &imageHeight);
+		do {
+			ar.ReadString(buf, 255);
+		} while (buf[0] == '#');
+
+		sscanf_s(buf, "%d", &maxValue);
+
+
+		if (strcmp(type, "P5") == 0)depth = 1;
+		else depth = 3;
+	}
+	else if (strcmp(strchr(fname, '.'), ".raw") == 0 ||
+		strcmp(strchr(fname, '.'), ".RAW") == 0) {
+		if (fp->GetLength() != 256 * 256) {
+			AfxMessageBox(CString("256*256크기의 파일만 사용가능합니다."));
+			return;
+		}
+		imageWidth = 256;
+		imageHeight = 256;
+		depth = 1;
+	}
+	inputImg = (unsigned char**)malloc(imageHeight * sizeof(unsigned char*));
+	resultImg = (unsigned char**)malloc(imageHeight * sizeof(unsigned char*));
+
+	for (i = 0; i < imageHeight; i++) {
+		inputImg[i] = (unsigned char*)malloc(imageWidth * depth);
+		resultImg[i] = (unsigned char*)malloc(imageWidth * depth);
+	}
+
+	for (i = 0; i < imageHeight; i++) {
+		ar.Read(inputImg[i], imageWidth * depth);
+	}
+}
---
+++ 04_01/ImagePro_kimdahunDoc.h
@@ -0,0 +1,65 @@
+﻿
+// ImagePro_kimdahunDoc.h: CImageProkimdahunDoc 클래스의 인터페이스
+//
+
+
+#pragma once
+
+
+class CImageProkimdahunDoc : public CDocument
+{
+protected: // serialization에서만 만들어집니다.
+	CImageProkimdahunDoc() noexcept;
+	DECLARE_DYNCREATE(CImageProkimdahunDoc)
+
+// 특성입니다.
+public:
+	unsigned char**inputImg;
+	unsigned char** inputimg2;
+
+	unsigned char** resultImg;
+	unsigned char** resultImg1;
+	unsigned char** resultImg2;
+
+	int imageWidth;
+	int imageHeight;
+	int depth;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// 구현입니다.
+public:
+	virtual ~CImageProkimdahunDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// 검색 처리기에 대한 검색 콘텐츠를 설정하는 도우미 함수
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+public:
+	void PixelAdd();
+	void Pixeltimes();
+	void PixelHistoEq();
+	void PixelTwoImageAdd();
+	void LoadTwoImages();
+	void LoadImageFile(CArchive& ar);
+};
