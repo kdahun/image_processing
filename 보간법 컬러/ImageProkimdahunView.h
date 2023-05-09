a682c198a2ab9c2b698b90554fa2bd557851d283
kdahun <101082485+kdahun@users.noreply.github.com>
2023-05-09 17:45:50 +0900

    보간법 컬러
    곱하기 먼저가 아닌 나누기 먼저

---
+++ 보간법 컬러/ImageProkimdahunView.h
@@ -0,0 +1,69 @@
+﻿
+// ImageProkimdahunView.h: CImageProkimdahunView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CImageProkimdahunView : public CScrollView
+{
+protected: // serialization에서만 만들어집니다.
+	CImageProkimdahunView() noexcept;
+	DECLARE_DYNCREATE(CImageProkimdahunView)
+
+// 특성입니다.
+public:
+	CImageProkimdahunDoc* GetDocument() const;
+	int viewMode;	
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void OnInitialUpdate(); // 생성 후 처음 호출되었습니다.
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CImageProkimdahunView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnPixelAdd();
+	afx_msg void OnPixelHistoEq();
+	afx_msg void OnPixelTwoImageAdd();
+	afx_msg void OnRegionSharpening();
+	afx_msg void OnRegionMeaning();
+	afx_msg void OnRegionSobel();
+	afx_msg void OnRegionMedian();
+	afx_msg void OnErosion();
+	afx_msg void OnDilattion();
+	afx_msg void OnOpening();
+	afx_msg void OnClosing();
+	afx_msg void OnGeometryZoominPixelCopy();
+	afx_msg void OnGeometryXoominInterpolation();
+	void GeometryZoomminInterpolation();
+};
+
+#ifndef _DEBUG  // ImageProkimdahunView.cpp의 디버그 버전
+inline CImageProkimdahunDoc* CImageProkimdahunView::GetDocument() const
+   { return reinterpret_cast<CImageProkimdahunDoc*>(m_pDocument); }
+#endif
+
